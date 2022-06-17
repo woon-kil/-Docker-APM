6c37f4157e330b892d1998e17129e222d5cd21e5
woon-kil <79067466+woon-kil@users.noreply.github.com>
2022-06-17 17:43:07 +0900

    c

--- app/test.mydomain.com/index.php
+++ app/test.mydomain.com/index.php
@@ -1,6 +1,5 @@
 <?php
-
-echo 'Hello World!'.PHP_EOL;
+echo 'Hello World!' . PHP_EOL;
 
 $servername = getenv('MYSQL_IP');
 $username = getenv('MYSQL_ROOT_USER');
@@ -8,7 +7,9 @@ $password = getenv('MYSQL_ROOT_PASSWORD');
 
 $conn = mysqli_connect($servername, $username, $password);
 if (!$conn) {
-   exit('Connection failed: '.mysqli_connect_error().PHP_EOL);
+   exit('Connection failed: ' . mysqli_connect_error() . PHP_EOL);
 }
 
-echo 'Successful database connection!'.PHP_EOL;
+echo 'Hello ~~ Successful database connection!' . PHP_EOL;
+phpinfo();
+exit;
